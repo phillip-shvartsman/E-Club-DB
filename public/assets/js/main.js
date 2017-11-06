8e97dfdbce0943aa480602250c8169b24c63dcfa
phillip-shvartsman <shvartsman.2@osu.edu>
2017-11-06 14:49:56 -0500

    Added Static Website on Top of DB

---
+++ public/assets/js/main.js
@@ -0,0 +1,135 @@
+var apiCall = "https://api.flickr.com/services/feeds/photos_public.gne?id=132038343@N02&tags=featured&lang=en-us&format=json&jsoncallback=?";
+
+
+
+// Back to Top
+
+$(document).ready(function() {
+
+
+  $(window).scroll(function() {
+
+    if ($(this).scrollTop() > 200) {
+      $('.go-top').fadeIn(200);
+    } else {
+      $('.go-top').fadeOut(200);
+    }
+  });
+
+  // Animate the scroll to top
+  $('.go-top').click(function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: 0
+    }, 300);
+  });
+
+
+
+
+  $('#brand, #brand-alt').removeClass('hidden');
+  $('#brand, #brand-alt').addClass('animated fadeIn');
+
+  $("#navigation").removeClass("hidden");
+  $("#navigation").addClass("animated fadeIn");
+
+  $('#landing').removeClass('hidden');
+  $('#landing').addClass('animated fadeIn');
+
+  $('#description').removeClass('hidden');
+  $('#description').addClass('animated zoomIn');
+
+  $('#quick-start').removeClass('hidden');
+  $('#quick-start').addClass('animated fadeIn');
+
+  $('#meeting-time').removeClass('hidden');
+  $('#meeting-time').addClass('animated fadeIn');
+
+  $('#featured').removeClass('hidden');
+  $('#featured').addClass('animated fadeIn');
+
+  $('#special-event').removeClass('hidden');
+  $('#special-event').addClass('animated fadeIn');
+
+  $('#officers').removeClass('hidden');
+  $('#officers').addClass('animated fadeIn');
+
+  $.getJSON(apiCall, function(data) {
+    $.each(data.items, function(i, item) {
+      $("<img data-u='image'/>").attr("src", item.media.m).appendTo(".flickr-stream")
+        .wrap("<div>" + "</div>");
+    });
+  });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+// Calendar
+
+
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+
+    defaultView: 'month',
+
+    views: {
+  listDay: { buttonText: 'month' },
+  listWeek: { buttonText: 'list for this week' }
+},
+    aspectRatio: 1.5,
+
+
+    eventClick: function(event) {
+      // opens events in a popup window
+      window.open(event.url, 'gcalevent', 'width=700,height=600');
+      return false;
+    },
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,listWeek'
+    },
+    contentHeight: '400',
+
+    eventTextColor: 'white',
+
+    googleCalendarApiKey: 'AIzaSyBu5kL2kgj9AZXCM8kfSLukPx1Vb7bRSh4',
+    events: {
+      googleCalendarId: 'electronicsosu@gmail.com',
+      timezone: 'America/New_York',
+    }
+  });
+});
+
+
+
+
+
+
+
+//$(document).ready(function() {
+//		$('.brand').addClass('animated bounceInUp');
+//});
+
+
+
+
+
+
+
+
+
+// jQuery(document).ready( function(){
+//    //jQuery('#flash').fadeIn(2000);
+//$('#brand img').addClass('animated zoominDown');
+//} );
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 var db = require('mongodb').MongoClient;
 var db_url = "mongodb://localhost:27017/e-club-db";
+var path = require("path");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	/*var inventory_res;
+	db.connect(db_url,function(err,db){
+		inventory_res = db.collection('inventory').find().toArray(function(err,docs){
+			res.render('index', { title: 'Electronics Club @ OSU', data:docs});
+		});; 	
+	});*/
+	res.sendFile(path.join(__dirname+'/old-site/index.html'));
+});
+router.get('/lab-resources',function(req,res,next){
 	var inventory_res;
 	db.connect(db_url,function(err,db){
 		inventory_res = db.collection('inventory').find().toArray(function(err,docs){
@@ -11,13 +22,17 @@ router.get('/', function(req, res, next) {
 		});; 	
 	});
 });
-
 router.post('/add', function(req, res, next){
 	console.log("We got a request!");
 	console.log(req.body);
 	res.send("Got your message");
 });
-
+router.get('/officers',function(req,res,next){
+	res.sendFile(path.join(__dirname+'/old-site/officers.html'))
+});
+router.get('/calendar',function(req,res,next){
+	res.sendFile(path.join(__dirname+'/old-site/calendar.html'))
+});
 module.exports = router;
 
 
